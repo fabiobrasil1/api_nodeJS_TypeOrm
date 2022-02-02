800549154b9834150680ccd9de24110d435ced8f
Fabio Brasil <fabiobfelix@hotmail.com>
2022-02-02 01:36:56 -0300

    hot fix server use express.json

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,10 @@
 import express from 'express';
 import { router } from './controllers/routes'
 
-
 const server = express();
 
-server.use(router)
-
+server.use(express.json());
+server.use(router);
 
 server.listen(3002, () => {
     console.log('servidor rodando na porta 3002 http://localhost:3002/')
