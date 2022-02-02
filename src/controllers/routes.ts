c9d215c9a5bb1d6af0dc7a11cbff825501fa263c
Fabio Brasil <fabiobfelix@hotmail.com>
2022-02-02 01:34:40 -0300

    update routes

--- src/controllers/routes.ts
+++ src/controllers/routes.ts
@@ -1,23 +1,15 @@
 import { Router, Request, Response } from 'express';
 import { CreateUserController } from './CreateUserController';
 
-
-
-
 const router = Router();
-
-
-
 const createUserController = new CreateUserController();
 
-
+// aplicação rodando
 router.get('/', (request:Request, response: Response) => {
-
+    
     return response.json({mensagem: 'Api rodando'})
-
 })
-
+//cria usuário
 router.post('/usuarios/', createUserController.handle)
-
 
 export { router }
