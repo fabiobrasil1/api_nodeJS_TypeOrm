0fb2772f445ea106fff72c25aa7622444d372b35
Fabio Brasil <fabiobfelix@hotmail.com>
2022-02-02 18:15:47 -0300

    update createUserController

--- src/controllers/CreateUserController.ts
+++ src/controllers/CreateUserController.ts
@@ -1,19 +1,25 @@
 import { Request, Response } from 'express';
+import { CreateUserService } from '../services/CreateUserService'
 
 class CreateUserController {
     handle(request: Request, response: Response) {
         
-         const data = request.body;
+        const data = request.body;
+        
+        const createUserService = new CreateUserService()
 
         const nome = data.nome;
         const idade = data.idade;
+        const email = data.email;
         const profissao = data.profissao
+      
+        if( nome.length === 0 || idade.length === 0 || email.length === 0 || profissao.length === 0){
+            return response.status(400).json({mensagem: 'preencha todos os campos'});
+        }
+       
+        const user = createUserService.execute({nome,idade,email,profissao})
 
-        return response.json({
-            "nome": nome,
-            "idade": idade,
-            "profissao": profissao,
-        }) 
+        return response.status(201).json({user}) 
     }
 }
 
